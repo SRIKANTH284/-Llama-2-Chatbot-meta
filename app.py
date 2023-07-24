29c1450740946bb2e7c45b319c498c0acd9d433b
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-07-24 19:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def generate_llama2_response(prompt_input):
     return output
 
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+prompt = st.text_input("Enter your message...", disabled=not replicate_api)
+if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
