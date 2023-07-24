5707e9d6153c6d06c64c3b798c9278b23010fe62
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-07-24 18:05:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from replicate
 import os
+from transformers import pipeline
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
@@ -26,12 +26,11 @@ with st.sidebar:
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     else:
         llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
-    
+
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
-    
-    
+
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
@@ -54,10 +53,11 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = run(llm, 
-                 input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                        "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
-    return output
+
+    # Using the pipeline from the transformers library to interact with the Llama model
+    generator = pipeline(model=llm, tokenizer=llm)
+    output = generator(f"{string_dialogue} {prompt_input} Assistant:", temperature=temperature, top_p=top_p, max_length=max_length, repetition_penalty=1)
+    return output[0]['generated']
 
 # User-provided prompt
 prompt = st.text_input("Enter your message:")
@@ -69,9 +69,6 @@ if prompt:
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.spinner("Thinking..."):
         response = generate_llama2_response(prompt)
-        full_response = ''
-        for item in response:
-            full_response += item
-        st.text(full_response)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+        st.text(response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)
