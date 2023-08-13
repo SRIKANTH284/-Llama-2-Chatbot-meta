124ed58b2b5f2125ae98bbbce3f15479e735817f
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-08-13 12:10:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🦙💬 Srikanth's Llama 2 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬srikanth's Llama 2 Chatbot')
+    st.title('🦙💬 Llama 2 Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
